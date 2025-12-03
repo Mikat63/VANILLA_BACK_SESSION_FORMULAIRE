27b83316c66e3e675c0cb29f2eab0058ccb3ae04
mikaT <m.tores63@outlook.fr>
2025-12-03 15:09:11 +0100

    COMPLETED : PHP-partie-6

--- PHP-partie-6/Exercice_6/index.php
+++ PHP-partie-6/Exercice_6/index.php
@@ -1,1 +1,37 @@
 <?php
+if (isset($_GET['building']) && !empty(trim($_GET['building']))) {
+    $building = "Building : " . htmlspecialchars($_GET['building']);
+} else {
+    $building = "Error : building not found";
+}
+
+if (isset($_GET['room']) && !empty(trim($_GET['room']))) {
+    $room = "room : " . htmlspecialchars($_GET['room']);
+} else {
+    $room = "Error : room not found";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP partie 6 - Exercice 6</title>
+</head>
+
+<body>
+    <div>
+        <p class="<?php echo ($building === "Error : building not found") ? 'red' : 'green'; ?>">
+            <?php echo $building ?>
+        </p>
+
+        <p class="<?php echo ($room === "Error : room not found") ? 'red' : 'green'; ?>">
+            <?php echo $room ?>
+        </p>
+    </div>
+</body>
+
+</html>
