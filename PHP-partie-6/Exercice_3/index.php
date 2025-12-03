7c5ef232fe0cedca822c08381c7bac4f5ecd0e0c
mikaT <m.tores63@outlook.fr>
2025-12-03 11:53:00 +0100

    CORRECTION : refact of php-partie-6 exercice_3

--- PHP-partie-6/Exercice_3/index.php
+++ PHP-partie-6/Exercice_3/index.php
@@ -1,4 +1,33 @@
 <?php
-if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
-    var_dump($_GET);
+$startDate = "";
+$endDate = "";
+
+if (isset($_GET['startDate']) && !empty(trim($_GET['startDate']))) {
+    $startDate = "Date de début : " . htmlspecialchars($_GET['startDate']);
+} else {
+    $startDate = "Date de début : inconnue";
 }
+
+if (isset($_GET['endDate']) && !empty(trim($_GET['endDate']))) {
+    $endDate = "Date de fin : " . htmlspecialchars($_GET['endDate']);
+} else {
+    $endDate = "Date de fin : inconnue";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP partie 6 - Exercice 3</title>
+</head>
+
+<body>
+    <p>
+        <?php echo $startDate . "<br>" . $endDate ?>
+    </p>
+</body>
+
+</html>
